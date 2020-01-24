b314acccb59c46163e6529dbc6c42bf632b5068e
GChiara <girardelli.chiara@gmail.com>
2020-01-24 22:30:54 +0100

    Added project files and tab A work in progress

---
+++ public/index.php
@@ -0,0 +1,160 @@
+<html lang="en">
+<head>
+    <?php include 'gtag.php' ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IW SI</title>
+    <!-- Add twitter and og meta here -->
+    <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:500" rel="stylesheet">
+    <link rel="stylesheet" href="static/tab_a.css">
+</head>
+<body>
+    <div id="app" class="tabC">   
+      <?php include 'header.php' ?>
+      <div class="container-fluid dashboard-container-outer">
+        <div class="row dashboard-container">
+          <!-- ROW FOR INFO AND SHARE -->
+          <div class="col-md-12">
+            <div class="row">
+              <!-- INFO -->
+              <div class="col-md-8 chart-col" v-if="showInfo">
+                <div class="boxed-container description-container">
+                  <h1>Στοιχεία περιουσιακής κατάστασης</h1>
+                  <p>Lorem Ipsum <a href="#" @click="readMore = true">Read more</a>.</p>
+                  <p v-show="readMore">Lorem Ipsum</p>  
+                  <i class="material-icons close-btn" @click="showInfo = false">close</i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- CHARTS - FIRST ROW-->
+          <div class="col-md-4 chart-col">
+            <div class="boxed-container chart-container tab_a_1">
+              <chart-header :title="charts.party.title" :info="charts.party.info" ></chart-header>
+              <div class="chart-inner" id="party_chart"></div>
+            </div>
+          </div>
+          <div class="col-md-4 chart-col">
+            <div class="boxed-container chart-container tab_a_2">
+              <chart-header :title="charts.income.title" :info="charts.income.info" ></chart-header>
+              <div class="chart-inner" id="income_chart"></div>
+            </div>
+          </div>
+          <div class="col-md-4 chart-col">
+            <div class="boxed-container chart-container tab_a_3">
+              <chart-header :title="charts.topRevenue.title" :info="charts.topRevenue.info" ></chart-header>
+              <div class="chart-inner" id="topRevenue_chart"></div>
+            </div>
+          </div>
+
+          <!-- TABLE -->
+          <div class="col-12 chart-col">
+            <div class="boxed-container chart-container chart-container-table">
+              <chart-header :title="charts.mainTable.title" :info="charts.mainTable.info" ></chart-header>
+              <div class="chart-inner chart-table">
+                <table class="table table-hover dc-data-table" id="dc-data-table">
+                  <thead>
+                    <tr class="header">
+                      <th class="header">Nr</th> 
+                      <th class="header">Ονομα</th>
+                      <th class="header">Πολιτικό κόμμα</th>
+                      <th class="header">συνολικό εισόδημα</th>
+                      <th class="header">Αριθμός πηγών εσόδω</th>
+                      <th class="header">Ημερομηνία της δήλωσης</th>
+                      <th class="header">Υψηλότερη πηγή εισοδήματος</th> 
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- DETAILS MODAL -->
+      <div class="modal" id="detailsModal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+              <div class="modal-title">
+                <div>{{ selectedElement.First_name }} {{ selectedElement.Last_name }}</div>
+                <div>{{ selectedElement.Party_GR }} </div>
+              </div>
+              <button type="button" class="close" data-dismiss="modal"><i class="material-icons">close</i></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="details-line"><span class="details-line-title">Ημερομηνία γέννησης:</span> </div>
+                    <div class="details-line"><span class="details-line-title">Προφίλ:</span> {{ selectedElement.Link_DOI }}</div>
+                    <div class="details-line" v-if="selectedElement.declaration"><span class="details-line-title">το σύνολο των δηλωθέντων εσόδων:</span> {{ selectedElement.declaration.totRevenue }}</div>
+                    <div class="details-line" v-if="selectedElement.declaration"><span class="details-line-title">Περιουσιακά στοιχεία που δηλώνονται από σύζυγο:</span> {{ selectedElement.declaration.totRevenueSpouse }}</div>
+                    <div class="details-line"><span class="details-line-title">όλα τα δηλωθέντα έσοδα</span></div>
+                    <table class="modal-table" v-if="selectedElement.declaration">
+                      <thead>
+                        <tr>
+                          <th>Ο ΑΠΟΚΤΩΝ ΤΟ ΕΙΣΟΔΗΜΑ</th>
+                          <th>ΕΙΔΟΣ ΕΙΣΟΔΟΥ</th>
+                          <th>ΠΟΣΟ</th>
+                          <th>ΝΟΜΙΣΜΑ</th>
+                          <th>ΠΑΡΑΤΗΡΗΣΕΙΣ</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="rev in selectedElement.declaration.revenue">
+                          <td>{{ rev.receipt }}</td>
+                          <td>{{ rev.type }}</td>
+                          <td>{{ rev.amount }}</td>
+                          <td>{{ rev.currency }}</td>
+                          <td>{{ rev.comments }}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Bottom bar -->
+      <div class="container-fluid footer-bar">
+        <div class="row">
+          <div class="footer-col col-12 col-sm-12 footer-counts">
+            <div class="dc-data-count count-box">
+              <div class="filter-count">0</div>από τους <strong class="total-count">0</strong> βουλευτές
+            </div>
+            <div class="footer-input">
+              <input type="text" id="search-input" placeholder="Αναζήτηση">
+              <i class="material-icons">search</i>
+            </div>
+          </div>
+        </div>
+        <!-- Reset filters -->
+        <button class="reset-btn"><i class="material-icons">settings_backup_restore</i><span class="reset-btn-text">επαναφορά</span></button>
+        <div class="footer-buttons-right">
+          <button @click="downloadDataset"><i class="material-icons">cloud_download</i></button>
+          <button class="btn-twitter" @click="share('twitter')"><img src="./images/twitter.png" /></button>
+          <button class="btn-fb" @click="share('facebook')"><img src="./images/facebook.png" /></button>
+        </div>
+      </div>
+      <!-- Loader -->
+      <loader v-if="loader" :text="'Loading ...'" />
+    </div>
+
+    <script type="text/javascript" src="vendor/js/d3.v5.min.js"></script>
+    <script type="text/javascript" src="vendor/js/d3.layout.cloud.js"></script>
+    <script type="text/javascript" src="vendor/js/crossfilter.min.js"></script>
+    <script type="text/javascript" src="vendor/js/dc.js"></script>
+    <script type="text/javascript" src="vendor/js/dc.cloud.js"></script>
+    <script src="static/tab_a.js"></script>
+
+ 
+</body>
+</html>
---
+++ src/tab_a.js
@@ -0,0 +1,626 @@
+import jquery from 'jquery';
+window.jQuery = jquery;
+window.$ = jquery;
+require( 'datatables.net' )( window, $ )
+require( 'datatables.net-dt' )( window, $ )
+
+import underscore from 'underscore';
+window.underscore = underscore;
+window._ = underscore;
+
+import '../public/vendor/js/popper.min.js'
+import '../public/vendor/js/bootstrap.min.js'
+import { csv } from 'd3-request'
+import { json } from 'd3-request'
+
+import '../public/vendor/css/bootstrap.min.css'
+import '../public/vendor/css/dc.css'
+import '/scss/main.scss';
+
+import Vue from 'vue';
+import Loader from './components/Loader.vue';
+import ChartHeader from './components/ChartHeader.vue';
+
+
+// Data object - is also used by Vue
+
+var vuedata = {
+  page: 'tabB',
+  loader: true,
+  readMore: false,
+  showInfo: true,
+  showShare: true,
+  showAllCharts: true,
+  chartMargin: 40,
+  instFilter: 'all',
+  charts: {
+    party: {
+      title: 'Πολιτικό κόμμα',
+      info: ''
+    },
+    income: {
+      title: 'Ετήσια δηλωμένα έσοδα',
+      info: ''
+    },
+    topRevenue: {
+      title: 'υψηλότερο εισόδημα - top 10',
+      info: ''
+    },
+    mainTable: {
+      chart: null,
+      type: 'table',
+      title: 'Table',
+      info: ''
+    }
+  },
+  selectedElement: { "P": "", "Sub": ""},
+  modalShowTable: '',
+  colors: {
+    generic: ["#3b95d0", "#4081ae", "#406a95", "#395a75" ],
+    default1: "#2b90b8",
+    pie1: {
+      "Poslovni subjekt": "#0d506b",
+      "Kmetijsko gospodarstvo": "#1d7598"
+    },
+    pie2: {
+      "Neomejeno": "#0d506b",
+      "Omejeno": "#1d7598"
+    }
+  }
+}
+
+
+
+//Set vue components and Vue app
+
+Vue.component('chart-header', ChartHeader);
+Vue.component('loader', Loader);
+
+new Vue({
+  el: '#app',
+  data: vuedata,
+  methods: {
+    //Share
+    downloadDataset: function () {
+      window.open('./data/tab_b/parliament.csv');
+    },
+    share: function (platform) {
+      if(platform == 'twitter'){
+        var thisPage = window.location.href.split('?')[0];
+        var shareText = 'Share text here ' + thisPage;
+        var shareURL = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(shareText);
+        window.open(shareURL, '_blank');
+        return;
+      }
+      if(platform == 'facebook'){
+        var toShareUrl = 'https://integritywatch.si';
+        var shareURL = 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(toShareUrl);
+        window.open(shareURL, '_blank', 'toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250,top=300,left=300');
+        return;
+      }
+    }
+  }
+});
+
+//Initialize info popovers
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+//Charts
+var charts = {
+  party: {
+    chart: dc.rowChart("#party_chart"),
+    type: 'row',
+    divId: 'party_chart'
+  },
+  income: {
+    chart: dc.pieChart("#income_chart"),
+    type: 'pie',
+    divId: 'income_chart'
+  },
+  topRevenue: {
+    chart: dc.rowChart("#topRevenue_chart"),
+    type: 'row',
+    divId: 'topRevenue_chart'
+  },
+  mainTable: {
+    chart: null,
+    type: 'table',
+    divId: 'dc-data-table'
+  }
+}
+
+//Functions for responsivness
+var recalcWidth = function(divId) {
+  return document.getElementById(divId).offsetWidth - vuedata.chartMargin;
+};
+var recalcWidthWordcloud = function() {
+  //Replace element if with wordcloud column id
+  var width = document.getElementById("party_chart").offsetWidth - vuedata.chartMargin*2;
+  return [width, 550];
+};
+var recalcCharsLength = function(width) {
+  return parseInt(width / 8);
+};
+var calcPieSize = function(divId) {
+  var newWidth = recalcWidth(divId);
+  if(newWidth > 400) {
+    newWidth = 400;
+  }
+  var sizes = {
+    'width': newWidth,
+    'height': 0,
+    'radius': 0,
+    'innerRadius': 0,
+    'cy': 0,
+    'legendY': 0
+  }
+  if(newWidth < 300) { 
+    sizes.height = newWidth + 170;
+    sizes.radius = (newWidth)/2;
+    sizes.innerRadius = (newWidth)/4;
+    sizes.cy = (newWidth)/2;
+    sizes.legendY = (newWidth) + 30;
+  } else {
+    sizes.height = newWidth*0.75 + 170;
+    sizes.radius = (newWidth*0.75)/2;
+    sizes.innerRadius = (newWidth*0.75)/4;
+    sizes.cy = (newWidth*0.75)/2;
+    sizes.legendY = (newWidth*0.75) + 30;
+  }
+  return sizes;
+};
+var resizeGraphs = function() {
+  for (var c in charts) {
+    var sizes = calcPieSize(charts[c].divId);
+    var newWidth = recalcWidth(charts[c].divId);
+    var charsLength = recalcCharsLength(newWidth);
+    if(charts[c].type == 'row'){
+      charts[c].chart.width(newWidth);
+      charts[c].chart.label(function (d) {
+        var thisKey = d.key;
+        if(thisKey.indexOf('###') > -1){
+          thisKey = thisKey.split('###')[0];
+        }
+        if(thisKey.length > charsLength){
+          return thisKey.substring(0,charsLength) + '...';
+        }
+        return thisKey;
+      })
+      charts[c].chart.redraw();
+    } else if(charts[c].type == 'bar') {
+      charts[c].chart.width(newWidth);
+      charts[c].chart.rescale();
+      charts[c].chart.redraw();
+    } else if(charts[c].type == 'pie') {
+      charts[c].chart
+        .width(sizes.width)
+        .height(sizes.height)
+        .cy(sizes.cy)
+        .innerRadius(sizes.innerRadius)
+        .radius(sizes.radius)
+        .legend(dc.legend().x(0).y(sizes.legendY).gap(10).legendText(function(d) { 
+          var thisKey = d.name;
+          if(thisKey.length > charsLength){
+            return thisKey.substring(0, charsLength) + '...';
+          }
+          return thisKey;
+        }));
+      charts[c].chart.redraw();
+    } else if(charts[c].type == 'cloud') {
+      charts[c].chart.size(recalcWidthWordcloud());
+      charts[c].chart.redraw();
+    }
+  }
+};
+
+//Add commas to thousands
+function addcommas(x){
+  if(parseInt(x)){
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  return x;
+}
+//Parse amount floats with dot as decimal separator and comma as thousands separator
+function parseAmount(amount) {
+  var parseAmt = amount.replace(/\./g, '');
+  parseAmt = parseFloat(parseAmt.replace(/,/g, '.'));
+  return parseAmt;
+}
+//Get income category
+function getIncomeCategory(totAmt) {
+  if(totAmt == 0) {
+    return "Καθόλου εισόδημα";
+  } else if(totAmt <= 1000) {
+    return "1€ - 1.000€";
+  } else if(totAmt <= 5000) {
+    return "1.001—5.000€";
+  } else if(totAmt <= 10000) {
+    return "5.001—10.000€";
+  } else if(totAmt <= 50000) {
+    return "10.001€ - 50.000€";
+  } else if(totAmt <= 100000) {
+    return "50.001€ - 100.000€";
+  } else if(totAmt > 100000) {
+    return "> 100.000€";
+  }
+}
+//Custom date order for dataTables
+var dmy = d3.timeParse("%d/%m/%Y");
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+  "date-eu-pre": function (date) {
+    if(date.indexOf("Cancelled") > -1){
+      date = date.split(" ")[0];
+    }
+      return dmy(date);
+  },
+  "date-eu-asc": function ( a, b ) {
+      return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+  },
+  "date-eu-desc": function ( a, b ) {
+      return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+  }
+});
+
+//Generate random parameter for dynamic dataset loading (to avoid caching)
+var randomPar = '';
+var randomCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+for ( var i = 0; i < 5; i++ ) {
+  randomPar += randomCharacters.charAt(Math.floor(Math.random() * randomCharacters.length));
+}
+//Load data and generate charts
+var lobbyist_typeList = {}
+csv('./data/mp-list.csv?' + randomPar, (err, mps) => {
+  json('./data/declarations.json?' + randomPar, (err, declarations) => {
+    //Loop through list to get declaration
+    _.each(mps, function (d) {
+      d.declaration = {};
+      var thisDec = _.find(declarations, function (dec) { return dec.id == d.ID });
+      if(thisDec) {
+        d.declaration = thisDec;
+      }
+      //Loop through declarations data to apply fixes and calculations
+      if(d.declaration.id) {
+        //Get tot and top revenues
+        d.declaration.totRevenue = 0;
+        d.declaration.totRevenueSpouse = 0;
+        d.declaration.topRevenue = {};
+        d.declaration.revenueNum = 0;
+        _.each(d.declaration.revenue, function (r) {
+          if(r.receipt = "ΥΠΟΧΡΕΟΣ"){
+            d.declaration.revenueNum ++;
+            d.declaration.totRevenue += parseAmount(r.amount);
+            if(!d.declaration.topRevenue.amount || parseAmount(d.declaration.topRevenue.amount) < parseAmount(r.amount)) {
+              d.declaration.topRevenue = r;
+            }
+          } else if(r.receipt = "ΣΥΖΥΓΟΣ") {
+            d.declaration.totRevenueSpouse += parseAmount(r.amount);
+          }
+        });
+        d.declaration.totRevenue = d.declaration.totRevenue.toFixed(2);
+        d.declaration.newestDecDate = "/";
+        //Get income category
+        d.declaration.incomeCategory = getIncomeCategory(d.declaration.totRevenue);
+      }
+    });
+
+    //Set dc main vars. The second crossfilter is used to handle the travels stacked bar chart.
+    var ndx = crossfilter(mps);
+    var searchDimension = ndx.dimension(function (d) {
+        var entryString = ' ' + d.Last_name.toLowerCase() + ' ' + d.First_name.toLowerCase();
+        return entryString.toLowerCase();
+    });
+
+    //CHART 1
+    var createPartyChart = function() {
+      var chart = charts.party.chart;
+      var dimension = ndx.dimension(function (d) {
+        return d.Party_GR;
+      });
+      var group = dimension.group().reduceSum(function (d) {
+          return 1;
+      });
+      var width = recalcWidth(charts.party.divId);
+      var charsLength = recalcCharsLength(width);
+      chart
+        .width(width)
+        .height(500)
+        .margins({top: 0, left: 0, right: 0, bottom: 20})
+        .group(group)
+        .dimension(dimension)
+        .colorCalculator(function(d, i) {
+          return vuedata.colors.default1;
+        })
+        .label(function (d) {
+          if(d.key && d.key.length > charsLength){
+            return d.key.substring(0,charsLength) + '...';
+          }
+          return d.key;
+        })
+        .title(function (d) {
+            return d.key + ': ' + d.value;
+        })
+        .elasticX(true)
+        .xAxis().ticks(4);
+        //chart.xAxis().tickFormat(numberFormat);
+        chart.render();
+    }
+
+    //CHART 2
+    var createIncomeChart = function() {
+      var chart = charts.income.chart;
+      var dimension = ndx.dimension(function (d) {
+        if(d.declaration.incomeCategory) {
+          return d.declaration.incomeCategory; 
+        } else {
+          return "/";
+        }
+      });
+      var group = dimension.group().reduceSum(function (d) { return 1; });
+      var sizes = calcPieSize(charts.income.divId);
+      var order = ["Καθόλου εισόδημα", "1€ - 1.000€", "1.001—5.000€", "5.001—10.000€", "10.001€ - 50.000€", "50.001€ - 100.000€", "> 100.000€", "/"]
+      chart
+        .width(sizes.width)
+        .height(sizes.height)
+        .cy(sizes.cy)
+        .innerRadius(sizes.innerRadius)
+        .radius(sizes.radius)
+        .legend(dc.legend().x(0).y(sizes.legendY).gap(10).legendText(function(d) { 
+          var thisKey = d.name;
+          if(thisKey.length > 40){
+            return thisKey.substring(0,40) + '...';
+          }
+          return thisKey;
+        }))
+        .title(function(d){
+          var thisKey = d.key;
+          return thisKey + ': ' + d.value;
+        })
+        .dimension(dimension)
+        .ordering(function(d) { return order.indexOf(d.key)})
+        /*
+        .colorCalculator(function(d, i) {
+          return vuedata.colors.pie1[d.key];
+        })
+        */
+        .group(group);
+
+      chart.render();
+    }
+
+    //CHART 3
+    var createTopRevenueChart = function() {
+      var chart = charts.topRevenue.chart;
+      var dimension = ndx.dimension(function (d) {
+        return d.First_name + ' ' + d.Last_name;
+      });
+      var group = dimension.group().reduceSum(function (d) {
+        if(d.declaration.totRevenue) {
+          return d.declaration.totRevenue;
+        } else {
+          return 0;
+        }
+      });
+      var filteredGroup = (function(source_group) {
+        return {
+          all: function() {
+            return source_group.top(10).filter(function(d) {
+              return (d.value != 0);
+            });
+          }
+        };
+      })(group);
+      var width = recalcWidth(charts.topRevenue.divId);
+      var charsLength = recalcCharsLength(width);
+      chart
+        .width(width)
+        .height(500)
+        .margins({top: 0, left: 0, right: 0, bottom: 20})
+        .group(filteredGroup)
+        .dimension(dimension)
+        .colorCalculator(function(d, i) {
+          return vuedata.colors.default1;
+        })
+        .label(function (d) {
+          if(d.key && d.key.length > charsLength){
+            return d.key.substring(0,charsLength) + '...';
+          }
+          return d.key;
+        })
+        .title(function (d) {
+            return d.key + ': ' + d.value;
+        })
+        .elasticX(true)
+        .xAxis().ticks(4);
+        //chart.xAxis().tickFormat(numberFormat);
+        chart.render();
+    }
+    
+    //TABLE
+    var createTable = function() {
+      var count=0;
+      charts.mainTable.chart = $("#dc-data-table").dataTable({
+        "columnDefs": [
+          {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0,   
+            data: function ( row, type, val, meta ) {
+              return count;
+            }
+          },
+          {
+            "searchable": false,
+            "orderable": true,
+            "targets": 1,
+            "defaultContent":"N/A",
+            "data": function(d) {
+              //id,function,party,institution,date,contact_type,org_name,lobbyist_type,purpose,purpose_details
+              return d.First_name + ' ' + d.Last_name;
+            }
+          },
+          {
+            "searchable": false,
+            "orderable": true,
+            "targets": 2,
+            "defaultContent":"N/A",
+            "data": function(d) {
+              return d.Party_GR;
+            }
+          },
+          {
+            "searchable": false,
+            "orderable": true,
+            "targets": 3,
+            "defaultContent":"N/A",
+            "data": function(d) {
+              return d.declaration.totRevenue;
+            }
+          },
+          {
+            "searchable": false,
+            "orderable": true,
+            "targets": 4,
+            "defaultContent":"N/A",
+            "data": function(d) {
+              if(d.declaration.revenue) {
+                return d.declaration.revenue.length;
+              }
+              return '0';
+            }
+          },
+          {
+            "searchable": false,
+            "orderable": true,
+            "targets": 5,
+            "defaultContent":"N/A",
+            "data": function(d) {
+              if(d.declaration) {
+                return d.declaration.year;
+              }
+              return '/';
+            }
+          },
+          {
+            "searchable": false,
+            "orderable": true,
+            "targets": 6,
+            "defaultContent":"N/A",
+            "data": function(d) {
+              if(d.declaration.topRevenue && d.declaration.topRevenue.type) {
+                return d.declaration.topRevenue.type;
+              }
+              return '/';
+            }
+          },
+        ],
+        "iDisplayLength" : 25,
+        "bPaginate": true,
+        "bLengthChange": true,
+        "bFilter": false,
+        "order": [[ 1, "desc" ]],
+        "bSort": true,
+        "bInfo": true,
+        "bAutoWidth": false,
+        "bDeferRender": true,
+        "aaData": searchDimension.top(Infinity),
+        "bDestroy": true,
+      });
+      var datatable = charts.mainTable.chart;
+      datatable.on( 'draw.dt', function () {
+        var PageInfo = $('#dc-data-table').DataTable().page.info();
+          datatable.DataTable().column(0, { page: 'current' }).nodes().each( function (cell, i) {
+              cell.innerHTML = i + 1 + PageInfo.start;
+          });
+        });
+        datatable.DataTable().draw();
+        
+      $('#dc-data-table tbody').on('click', 'tr', function () {
+        var data = datatable.DataTable().row( this ).data();
+        vuedata.selectedElement = data;
+        $('#detailsModal').modal();
+      });
+    }
+    //REFRESH TABLE
+    function RefreshTable() {
+      dc.events.trigger(function () {
+        var alldata = searchDimension.top(Infinity);
+        charts.mainTable.chart.fnClearTable();
+        charts.mainTable.chart.fnAddData(alldata);
+        charts.mainTable.chart.fnDraw();
+      });
+    }
+
+    //SEARCH INPUT FUNCTIONALITY
+    var typingTimer;
+    var doneTypingInterval = 1000;
+    var $input = $("#search-input");
+    $input.on('keyup', function () {
+      clearTimeout(typingTimer);
+      typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    });
+    $input.on('keydown', function () {
+      clearTimeout(typingTimer);
+    });
+    function doneTyping () {
+      var s = $input.val().toLowerCase();
+      searchDimension.filter(function(d) { 
+        return d.indexOf(s) !== -1;
+      });
+      throttle();
+      var throttleTimer;
+      function throttle() {
+        window.clearTimeout(throttleTimer);
+        throttleTimer = window.setTimeout(function() {
+            dc.redrawAll();
+        }, 250);
+      }
+    }
+
+    //Reset charts
+    var resetGraphs = function() {
+      for (var c in charts) {
+        if(charts[c].type !== 'table' && charts[c].chart.hasFilter()){
+          charts[c].chart.filterAll();
+        }
+      }
+      searchDimension.filter(null);
+      $('#search-input').val('');
+      dc.redrawAll();
+    }
+    $('.reset-btn').click(function(){
+      resetGraphs();
+    })
+    
+    //Render charts
+    createPartyChart();
+    createIncomeChart();
+    createTopRevenueChart();
+    //createBusinessChart();
+    //createLimitationsChart();
+    createTable();
+
+    $('.dataTables_wrapper').append($('.dataTables_length'));
+
+    //Hide loader
+    vuedata.loader = false;
+
+    //COUNTERS
+    //Main counter
+    var all = ndx.groupAll();
+    var counter = dc.dataCount('.dc-data-count')
+      .dimension(ndx)
+      .group(all);
+    counter.render();
+    //Update datatables
+    counter.on("renderlet.resetall", function(c) {
+      RefreshTable();
+    });
+
+    //Window resize function
+    window.onresize = function(event) {
+      resizeGraphs();
+    };
+  })
+})
